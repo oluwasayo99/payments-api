2ac513cea1de9e056dc52bcdcf36174522236aea
Oluwasayo Akinkunmi <oluwasayo.akinkunmi@ng.ab-inbev.com>
2026-03-18 09:40:08 +0100

    add txn logs entity

---
+++ src/main/java/com/oluwasayo/guard/model/TransactionLog.java
@@ -0,0 +1,35 @@
+package com.oluwasayo.guard.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transaction_logs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cardNo;
+
+    private Long amount;
+
+    private String merchantId;
+
+    private String ipAddress;
+
+    private String status;
+
+    private Long executionTime;
+
+    @Column(insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
